cbd525421a7533a669de2bee81a5ae4d70085b66
mugitty <mugitty@users.noreply.github.com>
2019-09-27 15:06:36 -0700

    Update VersionInfo.java
    ported over latest updates

--- gopaintCommon/src/edu/usc/ksom/pm/panther/paintCommon/VersionInfo.java
+++ gopaintCommon/src/edu/usc/ksom/pm/panther/paintCommon/VersionInfo.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 University Of Southern California
+ * Copyright 2019 University Of Southern California
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -17,14 +17,24 @@ package edu.usc.ksom.pm.panther.paintCommon;
 
 import java.io.Serializable;
 
-/**
- *
- * @author muruganu
- */
+
 public class VersionInfo implements Serializable {
-    String versionId;
-    String name;
-    String releaseDate;
+    
+    public static final String COMP_OBJ_IS_NULL = "No Comparision information";
+    public static final String COMP_OBJ_DOES_NOT_HAVE_VERSION_ID = "Comparision information does not have version id";
+    public static final String COMP_OBJ_DOES_NOT_HAVE_NAME = "Comparision information does not have name";
+    public static final String COMP_OBJ_DOES_NOT_HAVE_RELEASE_DATE = "Comparision information does not have release date";
+    public static final String REF_OBJ_DOES_NOT_HAVE_VERSION_ID = "Reference information does not have version id";
+    public static final String REF_OBJ_DOES_NOT_HAVE_NAME = "Reference information does not have name";
+    public static final String REF_OBJ_DOES_NOT_HAVE_RELEASE_DATE = "Reference information does not have release date"; 
+    public static final String INFO_DIFF_FOR_VERSION_ID = "Information different for version id";
+    public static final String INFO_DIFF_FOR_NAME = "Information different for name";
+    public static final String INFO_DIFF_FOR_RELEASE_DATE = "Information different for date";    
+
+    
+    private String versionId;
+    private String name;
+    private String releaseDate;
 
     public String getVersionId() {
         return versionId;
@@ -48,6 +58,41 @@ public class VersionInfo implements Serializable {
 
     public void setReleaseDate(String releaseDate) {
         this.releaseDate = releaseDate;
+    }
+    
+    
+    public String compareTo(VersionInfo compInfo) {
+        if (null == compInfo) {
+            return COMP_OBJ_IS_NULL;
+        }
+        if (null != versionId && null == compInfo.versionId) {
+            return COMP_OBJ_DOES_NOT_HAVE_VERSION_ID;
+        }
+        if (null != name && null == compInfo.name) {
+            return COMP_OBJ_DOES_NOT_HAVE_NAME;
+        }
+        if (null != releaseDate && null == compInfo.releaseDate) {
+            return COMP_OBJ_DOES_NOT_HAVE_RELEASE_DATE;
+        }
+        if (null == versionId && null != compInfo.versionId) {
+            return REF_OBJ_DOES_NOT_HAVE_VERSION_ID;
+        }
+        if (null == name && null != compInfo.name) {
+            return REF_OBJ_DOES_NOT_HAVE_NAME;
+        }
+        if (null == releaseDate && null != compInfo.releaseDate) {
+            return REF_OBJ_DOES_NOT_HAVE_RELEASE_DATE;
+        }
+        if (null != versionId && null != compInfo.versionId && false == versionId.equals(compInfo.versionId)) {
+            return INFO_DIFF_FOR_VERSION_ID;
+        }
+        if (null != name && null != compInfo.name && false == name.equals(compInfo.name)) {
+            return INFO_DIFF_FOR_NAME;
+        }
+        if (null != releaseDate && null != compInfo.releaseDate && false == releaseDate.equals(compInfo.releaseDate)) {
+            return INFO_DIFF_FOR_RELEASE_DATE;
+        }
+        return null;
     }
     
 }
