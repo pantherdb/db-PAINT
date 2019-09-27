af8a1ac58288c6f821723baef3d933ba7d849c1c
mugitty <mugitty@users.noreply.github.com>
2019-09-27 15:27:46 -0700

    Update VersionManager.java
    Ported over updates

--- gopaintServer/src/com/sri/panther/paintServer/logic/VersionManager.java
+++ gopaintServer/src/com/sri/panther/paintServer/logic/VersionManager.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 University Of Southern California
+ * Copyright 2019 University Of Southern California
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -17,15 +17,16 @@ package com.sri.panther.paintServer.logic;
 
 import com.sri.panther.paintServer.database.DataIO;
 import com.sri.panther.paintServer.datamodel.ClassificationVersion;
+import com.sri.panther.paintServer.datamodel.FullGOAnnotVersion;
+import com.sri.panther.paintServer.datamodel.PantherVersion;
 import com.sri.panther.paintServer.util.ConfigFile;
 
-/**
- *
- * @author muruganu
- */
+
 public class VersionManager {
     private static VersionManager inst;
     private static ClassificationVersion cv;
+    private static FullGOAnnotVersion fgav;
+    private static PantherVersion pv;
     
     private VersionManager() {
         
@@ -34,16 +35,41 @@ public class VersionManager {
     public static synchronized VersionManager getInstance() {
         if (null != inst) {
             return inst;
-        }
-        
-        DataIO dataIO = new DataIO(ConfigFile.getProperty(ConfigFile.KEY_DB_JDBC_DBSID));
-        cv = dataIO.getCurVersionInfo();
+        }        
         inst = new VersionManager();
+        initClsVersion();
+        initFullGOVersion();
+        initPantherersion();
         return inst;
     }
     
-    public ClassificationVersion getCurrentVersion() {
+    public ClassificationVersion getClsVersion() {
         return cv;
     }
     
+    public FullGOAnnotVersion getFullGOAnnotVersion() {
+        return fgav;
+    }
+    
+    public PantherVersion getPantherVersion() {
+        return pv;
+    }
+    
+    private static ClassificationVersion initClsVersion() {
+        DataIO dataIO = new DataIO(ConfigFile.getProperty(ConfigFile.KEY_DB_JDBC_DBSID));
+        cv = dataIO.getCurVersionInfo();
+        return cv;
+    }
+    
+    private static FullGOAnnotVersion initFullGOVersion() {
+        DataIO dataIO = new DataIO(ConfigFile.getProperty(ConfigFile.KEY_DB_JDBC_DBSID));
+        fgav = dataIO.getFullGOVersion();
+        return fgav;
+    }
+    
+    private static PantherVersion initPantherersion() {
+        DataIO dataIO = new DataIO(ConfigFile.getProperty(ConfigFile.KEY_DB_JDBC_DBSID));
+        pv = dataIO.getPantherVersion();
+        return pv;
+    }
 }
