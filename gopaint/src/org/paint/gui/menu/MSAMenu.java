4640bb7c1c611cb2be2731a5800198df8d5d64c9
mugitty <mugitty@users.noreply.github.com>
2019-09-27 13:57:14 -0700

    Update MSAMenu.java
    Ported latest changes

--- gopaint/src/org/paint/gui/menu/MSAMenu.java
+++ gopaint/src/org/paint/gui/menu/MSAMenu.java
@@ -1,35 +1,30 @@
-/* 
- * 
- * Copyright (c) 2010, Regents of the University of California 
- * All rights reserved.
+/**
+ *  Copyright 2019 University Of Southern California
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Neither the name of the Lawrence Berkeley National Lab nor the names of its contributors may be used to endorse 
- * or promote products derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.paint.gui.menu;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 import org.apache.log4j.Logger;
 import org.bbop.framework.GUIManager;
-import org.bbop.swing.DynamicMenu;
 import org.paint.dialog.MSAColorDialog;
+import org.paint.gui.event.DomainChangeEvent;
+import org.paint.gui.event.DomainChangeListener;
 import org.paint.gui.event.EventManager;
 import org.paint.gui.event.FamilyChangeEvent;
 import org.paint.gui.event.FamilyChangeListener;
@@ -37,53 +32,93 @@ import org.paint.gui.msa.MSA;
 import org.paint.gui.msa.MSAPanel;
 import org.paint.main.PaintManager;
 
-public class MSAMenu extends DynamicMenu
-implements FamilyChangeListener {
+public class MSAMenu extends JMenu
+implements FamilyChangeListener, DomainChangeListener {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private static Logger log = Logger.getLogger("MSAMenu");
 
-	private static final String full_align = "Entire Alignment";
+	private static final String LABEL_FULL_ALIGN = "Entire Alignment";
+	private static final String LABEL_TRIMMED_ALIGN = "Trimmed Alignment";
+	private static final String LABEL_DOMAIN = "Domain";
+	private static final String LABEL_DOMAIN_TRIMMED = "Condensed Domain";        
 	//	private static final String match_align = "Condensed Alignment";
 	//	private static final String conserved = "Subfamily conserved";
 	private static final String weight = "Use weights";
+        
 	private static final String update = "Edit msa colors/thresholds";
 
-	private JCheckBoxMenuItem weightedItem;
-	private JCheckBoxMenuItem fullItem;
+	//private JMenuItem  weightedItem;
+	private JMenuItem fullItem;
+        private JMenuItem matchStatesItem;
+        private JMenuItem domainItem;
+        private JMenuItem domainTrimmedItem;
 
 	public MSAMenu() {
-		super("Alignment");
-		boolean full_length;
-		boolean use_weight;
-		boolean weighted;
+		super("MSA and Domain");
+//                ButtonGroup group = new ButtonGroup();
+//            MSAPanel msaPanel = PaintManager.inst().getMSAPanel();
+////            if (null != msaPanel) {
+//                MSA msa = msaPanel.getModel();
+//                if (msa != null) {
+                    fullItem = new JMenuItem(LABEL_FULL_ALIGN);
+//                    group.add(fullItem);
+                    add(fullItem);
+                    fullItem.setEnabled(false);
+                    fullItem.addActionListener(new MSAActionListener());
 
-		MSAPanel msa = PaintManager.inst().getMSA();
-		if (msa != null) {
-			full_length = msa.isFullLength();
-			use_weight = msa.haveWeights();
-			weighted = msa.isWeighted();
-		}
-		else {
-			full_length = true;;
-			use_weight = false;
-			weighted = false;
-		}
+                    matchStatesItem = new JMenuItem(LABEL_TRIMMED_ALIGN);
+//                    group.add(matchStatesItem);
+                    add(matchStatesItem);
+                    matchStatesItem.setEnabled(false);
+                    matchStatesItem.addActionListener(new MSAActionListener());
 
-		fullItem = new JCheckBoxMenuItem(full_align);
-		fullItem.setSelected(full_length);
-		fullItem.addActionListener(new MSAActionListener());
-		add(fullItem);
+                    domainItem = new JMenuItem(LABEL_DOMAIN);
+//                    group.add(domainItem);
+                    add(domainItem);
+                    domainItem.setEnabled(false);
+                    domainItem.addActionListener(new MSAActionListener());
+                    
+                    domainTrimmedItem = new JMenuItem(LABEL_DOMAIN_TRIMMED);
+//                    group.add(domainTrimmedItem);
+                    add(domainTrimmedItem);
+                    domainTrimmedItem.setEnabled(false);
+                    domainTrimmedItem.addActionListener(new MSAActionListener());                   
+                    
 
-		addSeparator();
+//                }
+//            }
+                
+//		boolean full_length;
+//		boolean use_weight;
+//		boolean weighted;
 
-		weightedItem = new JCheckBoxMenuItem(weight);
-		weightedItem.setSelected(weighted);
-		weightedItem.setEnabled(use_weight);
-		weightedItem.addActionListener(new MSAActionListener());
-		add(weightedItem);
+//		MSAPanel msa = PaintManager.inst().getMSA();
+//		if (msa != null) {
+//			full_length = msa.isFullLength();
+//			use_weight = msa.haveWeights();
+//			weighted = msa.isWeighted();
+//		}
+//		else {
+//			full_length = true;;
+//			use_weight = false;
+//			weighted = false;
+//		}
+
+//		fullItem = new JMenuItem(full_align);
+//		fullItem.setSelected(full_length);
+//		fullItem.addActionListener(new MSAActionListener());
+//		add(fullItem);
+//
+//		addSeparator();
+//
+//		weightedItem = new JCheckBoxMenuItem(weight);
+//		weightedItem.setSelected(weighted);
+//		weightedItem.setEnabled(use_weight);
+//		weightedItem.addActionListener(new MSAActionListener());
+//		add(weightedItem);
 		//
 		//		wtsItem = new JCheckBoxMenuItem(coloring);
 		//		wtsItem.setSelected(displayType == MSA.DISPLAY_TYPE_WTS);
@@ -97,20 +132,55 @@ implements FamilyChangeListener {
 		add(updateMSA);
 
 		/* So we can hide and show this menu based on what data is available */
-		EventManager.inst().registerFamilyListener(this);
+                EventManager ev = EventManager.inst();
+		ev.registerFamilyListener(this);
+                ev.registerDomainChangeListener(this);
 	}
 
-	public void updateMenu(MSAPanel msa) {
-		if (msa != null) {
-			boolean full_length = msa.isFullLength();
-			fullItem.setSelected(full_length);
-			// Only add this menu item, if sequence weights information is available
-			//			sfConHMMItem.setVisible(show);
-			weightedItem.setEnabled(msa.haveWeights());
-			weightedItem.setSelected(msa.isWeighted());			
-			// Allow saving of msa information, if book was opened locally
-		}
+	public void updateMenu(MSAPanel msaPanel) {
+            MSA msa = msaPanel.getModel();
+            if (null != msa) {
+                if (true == msa.haveFullAlignData()) {
+                    fullItem.setEnabled(true);
+                    fullItem.setSelected(true);
+                }
+                if (true == msa.haveTrimmedAlignData()) {
+                    matchStatesItem.setEnabled(true);
+                    matchStatesItem.setSelected(false);
+                }
+                if (true == msa.haveDomainInfo()) {
+                    domainItem.setEnabled(true);
+                    domainItem.setSelected(false);
+                    domainTrimmedItem.setEnabled(true);
+                    domainTrimmedItem.setSelected(false);                    
+                }
+            }
+            else {
+                fullItem.setEnabled(false);
+                matchStatesItem.setEnabled(false);
+                domainItem.setEnabled(false);
+            }
+//		if (msaPanel != null) {
+//			boolean full_length = msaPanel.isFullLength();
+//			fullItem.setSelected(full_length);
+//			// Only add this menu item, if sequence weights information is available
+//			//			sfConHMMItem.setVisible(show);
+//			weightedItem.setEnabled(msaPanel.haveWeights());
+//			weightedItem.setSelected(msaPanel.isWeighted());			
+//			// Allow saving of msa information, if book was opened locally
+//		}
 	}
+
+
+    public void handleDomainChangeEvent(DomainChangeEvent event) {
+        MSAPanel msa = PaintManager.inst().getMSAPanel();
+        if (msa != null) {
+            setVisible(true);
+            updateMenu(msa);
+        } else {
+            setVisible(false);
+        }
+    }
 
 	/**
 	 * Class declaration
@@ -131,18 +201,42 @@ implements FamilyChangeListener {
 		 * @see
 		 */
 		public void actionPerformed(ActionEvent e) {
-			MSAPanel msa = PaintManager.inst().getMSA();
-			if (e.getSource().equals(fullItem)) {
-				msa.setFullLength(fullItem.isSelected());
-			} else if (e.getSource().equals(weightedItem)) {
-				msa.setWeighted(weightedItem.isSelected());
-			}
+			MSAPanel msaPanel = PaintManager.inst().getMSAPanel();
+                        if (null != msaPanel) {
+//                            System.out.println("MSA panel is valid");
+                            MSA msa = msaPanel.getModel();
+                            if (null == msa) {
+                                return;
+                            }
+//                            System.out.println("MSA model is valid");
+                            if (e.getSource().equals(fullItem) && true == fullItem.isEnabled()) {
+                                msa.setDisplayType(MSA.MSA_DISPLAY.ENTIRE_ALIGNMENT);
+                                msaPanel.setFullLength(true);                                
+//                                System.out.println("msa - switching to full length display");
+                            }
+                            else if (e.getSource().equals(matchStatesItem) && true == matchStatesItem.isEnabled()) {
+                                msa.setDisplayType(MSA.MSA_DISPLAY.TRIMMED);
+                                msaPanel.setFullLength(false);                                
+//                                System.out.println("msa - switching to trimmed display");                                
+                            }
+                            else if (e.getSource().equals(domainItem) && true == domainItem.isEnabled()) {
+                                msa.setDisplayType(MSA.MSA_DISPLAY.DOMAIN);
+                                msaPanel.setFullLength(true);                                
+//                                System.out.println("msa - switching to full length domain display");                 
+                            }
+                            else if (e.getSource().equals(domainTrimmedItem) && true == domainTrimmedItem.isEnabled()) {
+                                msa.setDisplayType(MSA.MSA_DISPLAY.DOMAIN_TRIMMED);
+                                msaPanel.setFullLength(true);                                
+//                                System.out.println("msa - switching to trimmed domain display");                                 
+                            }                            
+                        }
+
 		}
 	}
 
 	private class MSAUpdateActionListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			MSAPanel msa = PaintManager.inst().getMSA();
+			MSAPanel msa = PaintManager.inst().getMSAPanel();
 			MSAColorDialog dlg = new MSAColorDialog(GUIManager.getManager().getFrame(), msa.isWeighted());
 			if (dlg.display()) {
 				msa.updateColors();
@@ -151,7 +245,7 @@ implements FamilyChangeListener {
 	}
 
 	public void newFamilyData(FamilyChangeEvent e) {
-		MSAPanel msa = PaintManager.inst().getMSA();
+		MSAPanel msa = PaintManager.inst().getMSAPanel();
 		if (msa != null) {
 			setVisible(true);
 			updateMenu(msa);
