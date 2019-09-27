8d305b06a3b2ddd34024b6bd0f5c7837dd8e6f06
mugitty <mugitty@users.noreply.github.com>
2019-09-27 14:12:30 -0700

    Update EventManager.java
    Ported over latest updates

--- gopaint/src/org/paint/gui/event/EventManager.java
+++ gopaint/src/org/paint/gui/event/EventManager.java
@@ -1,21 +1,17 @@
-/* 
- * 
- * Copyright (c) 2010, Regents of the University of California 
- * All rights reserved.
+/**
+ *  Copyright 2019 University Of Southern California
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Neither the name of the Lawrence Berkeley National Lab nor the names of its contributors may be used to endorse 
- * or promote products derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.paint.gui.event;
 
@@ -64,6 +60,7 @@ public class EventManager {
 	private HashSet<CurationColorListener> colorChangeListeners;
 	private HashSet<AnnotationDragListener> annotationDragListeners;
         private HashSet<CommentChangeListener> commentChangeListeners;
+        private HashSet<DomainChangeListener> domainChangeListeners;
 
 	protected List<GeneNode> selectedNodes;
 	protected List<Term> term_selection;
@@ -228,6 +225,13 @@ public class EventManager {
                 commentChangeListeners = new HashSet<CommentChangeListener>();
             }
             commentChangeListeners.add(listener);
+        }
+        
+        public void registerDomainChangeListener(DomainChangeListener listener) {
+            if (null == domainChangeListeners) {
+                domainChangeListeners = new HashSet<DomainChangeListener>();
+            }
+            domainChangeListeners.add(listener);
         }
 
 	/**
@@ -573,11 +577,20 @@ public class EventManager {
         
         public void fireCommentChangeEvent(CommentChangeEvent event) {
             if (null == commentChangeListeners) {
-                return;
+                 return;
             }
             for (CommentChangeListener listener: commentChangeListeners) {
                 listener.handleCommentChangeEvent(event);
             }
+        }
+        
+        public void fireDomainChangeEvent(DomainChangeEvent event) {
+            if (null == domainChangeListeners) {
+                return;
+            }
+            for (DomainChangeListener listener: domainChangeListeners) {
+                listener.handleDomainChangeEvent(event);
+            }            
         }
 
 	/**
