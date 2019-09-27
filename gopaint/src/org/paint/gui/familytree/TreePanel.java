700a39205eb26af920eba27a302a55c7bba0022e
mugitty <mugitty@users.noreply.github.com>
2019-09-27 14:04:16 -0700

    Update TreePanel.java
    Ported over latest changes

--- gopaint/src/org/paint/gui/familytree/TreePanel.java
+++ gopaint/src/org/paint/gui/familytree/TreePanel.java
@@ -1,22 +1,19 @@
-/* 
- * 
- * Copyright (c) 2010, Regents of the University of California 
- * All rights reserved.
+/**
+ *  Copyright 2019 University Of Southern California
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Neither the name of the Lawrence Berkeley National Lab nor the names of its contributors may be used to endorse 
- * or promote products derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 
 package org.paint.gui.familytree;
 
@@ -632,7 +629,7 @@ AnnotationDragListener{
 		GeneNode dsn = getPopupNode(e);
 		if (dsn != null) {
 			popup = new JPopupMenu();
-			if (!dsn.isLeaf() && !dsn.isPruned()) {
+			if (!dsn.isLeaf()) {
 				JMenuItem menuItem;
 				if (dsn.isExpanded())
 					menuItem = new JMenuItem(POPUP_MENU_COLLAPSE);
@@ -642,7 +639,7 @@ AnnotationDragListener{
 				popup.add(menuItem);                            
 			}
 			// Now add the reroot to node information
-			if (!dsn.isLeaf() && !dsn.isPruned()) {
+			if (!dsn.isLeaf()) {
 				JMenuItem menuItem;
 				if (dsn != getCurrentRoot()) {
 					menuItem = new JMenuItem(POPUP_MENU_REROOT);
