649c08d9d1e0f9eb40f00294e8488f5f21e08623
mugitty <mugitty@users.noreply.github.com>
2019-09-27 14:09:45 -0700

    Update CommentChangeListener.java
    Ported over latest changes

--- gopaint/src/org/paint/gui/event/CommentChangeListener.java
+++ gopaint/src/org/paint/gui/event/CommentChangeListener.java
@@ -1,6 +1,5 @@
-package org.paint.gui.event;
 /**
- *  Copyright 2016 University Of Southern California
+ *  Copyright 2019 University Of Southern California
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,6 +13,7 @@ package org.paint.gui.event;
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+package org.paint.gui.event;
 import java.util.EventListener;
 
 public interface CommentChangeListener extends EventListener {
